b389558ea377446e52a0271dec1842fdea814831
Vasko <vaskopoh@gmail.com>
2026-02-22 22:48:06 +0200

    First commit

---
+++ src/main/java/com/example/lexdiff/LexDiffApplication.java
@@ -0,0 +1,13 @@
+package com.example.lexdiff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexDiffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LexDiffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lexdiff/LexDiffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lexdiff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexDiffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
